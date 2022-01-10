0369ed1f7ef87929b90e0d0b75f98a2bd7efd7bb
Elliot Schlegelmilch <elliot@schlegelmilch.org>
2022-01-10 15:17:02 -0800

    try {}.

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,12 +2,16 @@
 class yggdrasil::config {
 
   exec { 'generate config':
-    command => "yggdrasil -json -genconf > $vardir/yggdrasil.generated.json",
+    command => "yggdrasil -json -genconf > ${vardir}/yggdrasil.generated.json",
     path    => "/usr/bin:/usr/local/bin",
     creates => "$vardir/yggdrasil.generated.json",
     user    => 0,
     group   => 0,
   }
+
+  $json = loadjson("${vardir}/yggdrasil.generated.json")
+  $generated_public_key = $json['PublicKey']
+  $generated_private_key = $json['PrivateKey']
 
   file { $yggdrasil::config_file:
     ensure  => file,
