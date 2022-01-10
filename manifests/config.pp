f2412dbc3f6c5da38608c2ef8e6d89192afd0ef8
Elliot Schlegelmilch <elliot@schlegelmilch.org>
2022-01-10 14:31:04 -0800

    generate a keypair into a temporary location.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,13 @@
 #
 class yggdrasil::config {
 
+  exec { 'generate config':
+    command => "yggdrasilctl -json -genconf > $vardir/yggdrasil.generated.json",
+    creates => "$vardir/yggdrasil.generated.json",
+    user    => 0,
+    group   => 0,
+  }
+
   file { $yggdrasil::config_file:
     ensure  => file,
     owner   => 0,
