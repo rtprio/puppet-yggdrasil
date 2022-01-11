bd960b0571a353cb99df6f6f2e8325e20b230cd9
Elliot Schlegelmilch <elliot@schlegelmilch.org>
2022-01-10 16:13:59 -0800

    dump initial config, load it into template.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,15 +1,18 @@
 #
 class yggdrasil::config {
 
+  $yggvar = '/var/yggdrasil'
+  file { "$yggvar": ensure => directory }
+
   exec { 'generate config':
-    command => "yggdrasil -json -genconf > ${vardir}/yggdrasil.generated.json",
+    command => "yggdrasil -json -genconf > ${yggvar}/yggdrasil.generated.json",
     path    => "/usr/bin:/usr/local/bin",
-    creates => "$vardir/yggdrasil.generated.json",
+    creates => "$yggvar/yggdrasil.generated.json",
     user    => 0,
     group   => 0,
   }
 
-  $json = loadjson("${vardir}/yggdrasil.generated.json")
+  $json = loadjson("${yggvar}/yggdrasil.generated.json")
   $generated_public_key = $json['PublicKey']
   $generated_private_key = $json['PrivateKey']
 
