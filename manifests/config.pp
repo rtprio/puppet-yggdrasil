ddff1262871bbfada410b115e3c5d7d36ed9a733
Elliot Schlegelmilch <elliot@schlegelmilch.org>
2022-01-10 19:40:35 -0800

    move key creation and contain them.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,20 +1,6 @@
 #
 class yggdrasil::config {
-
-  $yggvar = '/var/yggdrasil'
-  file { "$yggvar": ensure => directory }
-
-  exec { 'generate config':
-    command => "yggdrasil -json -genconf > ${yggvar}/yggdrasil.generated.json",
-    path    => "/usr/bin:/usr/local/bin",
-    creates => "${yggvar}/yggdrasil.generated.json",
-    user    => 0,
-    group   => 0,
-  }
-
-  $json = loadjson("${yggvar}/yggdrasil.generated.json")
-  $generated_public_key = $json['PublicKey']
-  $generated_private_key = $json['PrivateKey']
+  contain yggdrasil::config::keys
 
   file { $yggdrasil::config_file:
     ensure  => file,
@@ -24,4 +10,5 @@ class yggdrasil::config {
     content => epp('yggdrasil/yggdrasil.conf.epp'),
   }
 
+  Class['yggdrasil::config::keys'] -> File[$yggdrasil::config_file]
 }
