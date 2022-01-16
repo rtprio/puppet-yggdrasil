3363626d749f3483348f64fd377e5baf1d7d3c8a
Elliot Schlegelmilch <elliot@schlegelmilch.org>
2022-01-16 14:23:30 -0800

    document.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,13 +1,18 @@
 # class yggdrasil::service
 #
+# @param [Variant[Boolean, Enum['true', 'false', 'running', 'stopped']]] yggdrasil_ensure
+#     run or stop the yggdrasil service.
+# @param [Boolean] yggdrasil_enable
+#     enable or disable the service on startup.
+#
 class yggdrasil::service(
   Variant[Boolean, Enum['true', 'false', 'running', 'stopped']] $yggdrasil_ensure = 'running',
   Boolean $yggdrasil_enable = true,
 ) {
 
-  # service { 'yggdrasil':
-  #   ensure => $yggdrasil::service::yggdrasil_running,
-  #   enable => $yggdrasil::service::yggdrasil_enable,
-  # }
+  service { 'yggdrasil':
+    ensure => $yggdrasil::service::yggdrasil_running,
+    enable => $yggdrasil::service::yggdrasil_enable,
+  }
 
 }
