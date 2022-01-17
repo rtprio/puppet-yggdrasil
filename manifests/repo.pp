1b89c610e2f26cb58cf9e5e35e63a2bf750b6835
Elliot Schlegelmilch <elliot@schlegelmilch.org>
2022-01-17 07:25:50 -0800

    why is linux so complicated.

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,29 +2,24 @@
 #
 class yggdrasil::repo {
 
-  case $facts['os']['family'] {
-    'Debian', 'Ubuntu': {
-      apt::source { 'neailalexandere':
-        comment  => 'Debian repo for yydrasil packages.',
-        location => 'http://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/',
-        release  => 'debian',
-        repos    => 'yggdrasil',
-        key      => {
-          'id'     => '569130E8CA20FBC4CB3FDE555898470A764B32C9',
-          'source' => 'https://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/key.txt',
-        },
-        include  => {
-          'deb' => true,
-        },
-      }
+  if $facts['kernel'] == 'Linux' {
+  if $facts['os']['family'] in ['Debian', 'Ubuntu'] {
+    apt::source { 'neailalexandere':
+      comment  => 'Debian repo for yggdrasil packages.',
+      location => 'http://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/',
+      release  => 'debian',
+      repos    => 'yggdrasil',
+      key      => {
+        'id'     => '569130E8CA20FBC4CB3FDE555898470A764B32C9',
+        'source' => 'https://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/key.txt',
+      },
+      include  => { 'deb' => true, },
     }
-
-    'RedHat', 'CentOS':  {
-      err("repo support for '${facts['os']['family']}' not implemented yet")
-    }
-
-    default: { }
-
   }
 
+  if $facts['os']['family'] in ['RedHat', 'CentOS'] {
+    err("repo support for '${facts['os']['family']}' not implemented yet")
+  }
+
+}
 }
