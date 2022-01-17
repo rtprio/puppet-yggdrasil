31c5b3de655c7f2d7059cd61b3872b978fe67dd9
Elliot Schlegelmilch <elliot@schlegelmilch.org>
2022-01-17 07:09:17 -0800

    catch if json can not parse; restructure output hash a bit.

--- lib/facter/yggdrasil.rb
+++ lib/facter/yggdrasil.rb
@@ -6,8 +6,17 @@ Facter.add(:yggdrasil) do
   setcode do
     if Facter::Util::Resolution.which('yggdrasilctl')
       output = Facter::Util::Resolution.exec('yggdrasilctl -json getself')
-
-      JSON.parse(output)
+      # output is { "self" :{ "<ip>": { info ...
+      # splice it a bit to be more facter-y
+      begin
+        json = JSON.parse(output)
+        myself = json['self']
+        myip = myself.keys[0]
+        myself[myip]['ip'] = myip
+        myself[myip]
+      rescue => error
+        Hash.new
+      end
     end
   end
 end
