60c103e47246407f90c168f629ec09d14cfaefac
Elliot Schlegelmilch <elliot@schlegelmilch.org>
2022-01-17 07:26:06 -0800

    cull extra tests until later.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,35 +1,9 @@
 require 'spec_helper'
 
 describe 'yggdrasil' do
-  let(:facts) { { "os" => {
-    "distro" => { "codename" => "buster", "id" => "Debian",
-    "release" => { "full" => "10.11", "major" => "10", "minor" => "11", } },
-    "family" => "Debian", "name" => "Debian",
-    "release" => { "full" => "10.11", "major" => "10", "minor" => "11", },
-  } } }
-    
   it { is_expected.to compile }
   it { is_expected.to contain_class('yggdrasil::install') }
   it { is_expected.to contain_class('yggdrasil::keys') }
   it { is_expected.to contain_class('yggdrasil::config') }
   it { is_expected.to contain_class('yggdrasil::service') }
-
-  let(:facts) { {
-  "os": {
-    "distro": {
-      "codename": "Ootpa",
-      "id": "RedHatEnterprise",
-      "release": { "full": "8.5", "major": "8", "minor": "5" } },
-    "family": "RedHat", "hardware": "x86_64", "name": "OracleLinux",
-    "release": { "full": "8.5", "major": "8", "minor": "5" },
-  }
-} }
-
-  
-  it { is_expected.to compile }
-  it { is_expected.to contain_class('yggdrasil::install') }
-  it { is_expected.to contain_class('yggdrasil::keys') }
-  it { is_expected.to contain_class('yggdrasil::config') }
-  it { is_expected.to contain_class('yggdrasil::service') }
-
 end
