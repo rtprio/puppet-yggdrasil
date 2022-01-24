38c7f7d892598513e8de0033ed6aa32db02f8e84
Elliot Schlegelmilch <elliot@schlegelmilch.org>
2022-01-23 20:44:40 -0800

    fail on redhat; no repo. correct tests to match. thanks rubocop.

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -3,23 +3,22 @@
 class yggdrasil::repo {
 
   if $facts['kernel'] == 'Linux' {
-  if $facts['os']['family'] in ['Debian', 'Ubuntu'] {
-    apt::source { 'neailalexandere':
-      comment  => 'Debian repo for yggdrasil packages.',
-      location => 'http://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/',
-      release  => 'debian',
-      repos    => 'yggdrasil',
-      key      => {
-        'id'     => '569130E8CA20FBC4CB3FDE555898470A764B32C9',
-        'source' => 'https://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/key.txt',
-      },
-      include  => { 'deb' => true, },
+    if $facts['os']['family'] in ['Debian', 'Ubuntu'] {
+      apt::source { 'neailalexandere':
+        comment  => 'Debian repo for yggdrasil packages.',
+        location => 'http://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/',
+        release  => 'debian',
+        repos    => 'yggdrasil',
+        key      => {
+          'id'     => '569130E8CA20FBC4CB3FDE555898470A764B32C9',
+          'source' => 'https://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/key.txt',
+        },
+        include  => { 'deb' => true, },
+      }
+    }
+
+    if $facts['os']['family'] in ['RedHat', 'CentOS'] {
+      fail("no repo exists for '${facts['os']['family']}'.")
     }
   }
-
-  if $facts['os']['family'] in ['RedHat', 'CentOS'] {
-    err("no repo exists for '${facts['os']['family']}'.")
-  }
-
-}
 }
